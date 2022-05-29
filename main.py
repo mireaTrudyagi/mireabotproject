654e9a92273f1c1bc891532995e75cfd8bd28343
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-05-30 00:07:48 +0300

    Update and rename mirea.py to main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiogram.utils import executor
+from create import dp
+from date_base import search
+
+search.firebase_start()
+
+async def on_startup(_):
+    print('bot is online')
+
+
+from handlers import client, other
+
+client.register_handlers_client(dp)
+other.register_handlers_other(dp)
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
