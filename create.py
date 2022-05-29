359eb74bdb087f92e7de226d67bc4d066a49e7ac
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-05-30 00:48:59 +0300

    Update create.py

--- create.py
+++ create.py
@@ -2,6 +2,6 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
 
-API_TOKEN = '5349702815:AAEiskDPRaO4C4Ye029BH04BFmujBCHdV0w'
+API_TOKEN = 'XXX'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
