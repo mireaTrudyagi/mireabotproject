45a42364f51b61426bb2ce68daf91e5662f57679
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-06-11 09:20:34 +0300

    Update create.py

--- create.py
+++ create.py
@@ -1,7 +1,8 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 API_TOKEN = 'XXX'
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
