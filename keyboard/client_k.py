f6f4ad5242d6ac698f3091f0b0c78ffbd8e9f9e7
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-06-12 15:52:33 +0300

    Update client_k.py

--- keyboard/client_k.py
+++ keyboard/client_k.py
@@ -1,10 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-b1 = KeyboardButton('Жанр')
-b2 = KeyboardButton('Исполнитель')
-b3 = KeyboardButton('Песня')
-b4 = KeyboardButton('Альбом')
+b1 = KeyboardButton('Жанр\U0001F3AD')
+b2 = KeyboardButton('Исполнитель\U0001F60E')
+b3 = KeyboardButton('Песня\U0001F3B5')
+b4 = KeyboardButton('Альбом\U0001F4C1')
 
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
 kb_client.add(b1).insert(b2).add(b3).insert(b4)
