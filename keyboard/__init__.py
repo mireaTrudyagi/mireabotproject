ed1cc695d2588906d732d785da443a3ea7605fba
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-06-12 15:52:13 +0300

    Update __init__.py

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -1,1 +1,5 @@
 from keyboard.client_k import kb_client
+from keyboard.song_k import kb_song
+from keyboard.album_k import kb_album
+from keyboard.genre_k import kb_genre
+from keyboard.singer_k import kb_singer
