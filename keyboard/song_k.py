5e30636734784331c0bef3f864d45ededde35e56
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-06-12 15:54:27 +0300

    Create song_k.py

---
+++ keyboard/song_k.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+b1 = KeyboardButton('Enemy\U0001F480')
+b2 = KeyboardButton('Shape of You\U0001F498')
+b3 = KeyboardButton('The Nights\U0001F319')
+b4 = KeyboardButton('Youre Gonna Go Far, Kid\U0001F92C')
+b5 = KeyboardButton('Главное меню\U0001F3E0')
+
+kb_song = ReplyKeyboardMarkup(resize_keyboard=True)
+
+kb_song.add(b1).insert(b2).add(b3).insert(b4).add(b5)
