15492553f08e3d87e43a9f9616e6a65647efc013
RogaJedi <104651521+RogaJedi@users.noreply.github.com>
2022-06-12 23:39:26 +0300

    Update song_k.py

--- keyboard/song_k.py
+++ keyboard/song_k.py
@@ -4,8 +4,9 @@ b1 = KeyboardButton('Enemy\U0001F480')
 b2 = KeyboardButton('Shape of You\U0001F498')
 b3 = KeyboardButton('The Nights\U0001F319')
 b4 = KeyboardButton('Youre Gonna Go Far, Kid\U0001F92C')
-b5 = KeyboardButton('Главное меню\U0001F3E0')
+b5 = KeyboardButton('Dynamite\U0001F4A5')
+b6 = KeyboardButton('Главное меню\U0001F3E0')
 
 kb_song = ReplyKeyboardMarkup(resize_keyboard=True)
 
-kb_song.add(b1).insert(b2).add(b3).insert(b4).add(b5)
+kb_song.add(b1).insert(b2).add(b3).insert(b4).insert(b5).add(b6)
