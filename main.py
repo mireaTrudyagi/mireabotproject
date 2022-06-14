902dacaddd40672c2e3b377e0b9d6b35c8a8432f
Dan Kireyev <104600762+RaiderOfJoke@users.noreply.github.com>
2022-06-14 15:44:09 +0300

    Телеграмм бот Киреев Д.
    Тестирование машины состояний

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
+#Тестовые модули:
+# Песня: Film
+# Исполнитель: Daft Punk
+# Жанр: Поп
+# Альбом: The works
+import logging
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils import executor
-from create import dp
-from handlers import client
+from aiogram.types import BotCommand
+from Messages import register_handlers_song
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
 
-storage = MemoryStorage()
+logger = logging.getLogger(__name__)
 
-client.firebase_start()
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
 
-async def on_startup(_):
-    print('bot is online')
 
-client.register_handlers_client(dp)
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="/song", description="Найти песню"),
+        BotCommand(command="/artist", description="Найти исполнителя"),
+        BotCommand(command="/genre", description="Поиск по жанру"),
+        BotCommand(command="/album", description="Найти альбом")
+    ]
+    await bot.set_my_commands(commands)
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logger.error("Starting bot")
+    storage = MemoryStorage()
+    # Токен бота
+    bot = Bot(token="5305638021:AAEW69hy3rstaidolehzj7DNPgDcbXqFkqU")
+    # Диспетчер для бота
+    dp = Dispatcher(bot, storage=MemoryStorage())
+
+    await set_commands(bot)
+
+    @dp.message_handler(commands=['help', 'start'])
+    async def cmd_start(message: types.Message):
+        await message.answer("Привет! Я музыкальный бот. В данный момент доступны такие команды: /song, /artist, /genre, /album")
+    register_handlers_song(dp)
+
+    await dp.start_polling()
+if __name__ == "__main__":
+    # Запуск бота
+    asyncio.run(main())
